eca3f8f90f5cd113fdd05657955e099a1dc11bca
Esteban Zuluaga <estebanc.zuluaga@gmail.com>
2021-09-18 18:04:18 -0500

    implementación de crud en la página

--- servidor.js
+++ servidor.js
@@ -1,48 +1,55 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const mysql = require('mysql');
-const bodyParser = require('body-parser');
-let alert = require('alert');
-session = require('express-session');
-const passport = require('passport');
+const mysql = require("mysql");
+const bodyParser = require("body-parser");
+let alert = require("alert");
+session = require("express-session");
+const passport = require("passport");
 //app.use(bodyParser());
 
-const net = require('net');
+const net = require("net");
 app.use(express.json());
-app.use(express.urlencoded({
-    extended: true
-}));
-app.use(session({
-    secret: 'secret',
-    resave: true,
-    saveUninitialized: true
-}));
+app.use(
+  express.urlencoded({
+    extended: true,
+  })
+);
 
-const path = require('path');
+app.use(
+  session({
+    secret: "secret",
+    resave: true,
+    saveUninitialized: true,
+  })
+);
+
+const path = require("path");
 let _message;
 app.listen(5000, () => {
-    console.log('server on port 5000')
+  console.log("server on port 5000");
 });
-app.use(bodyParser.urlencoded({
-    extended: true
-}));
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
 //MOTOR DE PLANTILLAS--------------------------------------
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
 const database = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "placas",
-    port: 8111
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "placas",
+  port: 8111,
 });
 
 database.connect((err) => {
-    if (err) {
-        throw err;
-    }
+  if (err) {
+    throw err;
+  }
 
-    console.log('Connected to DB');
+  console.log("Connected to DB");
 });
 //--------UDP----------------------
 /* const dgram = require('dgram');
@@ -57,18 +64,18 @@ socket.on('message', (msg, rinfo) => {
 }); */
 //---------------TCP----------------
 const server = net.createServer((socket) => {
-    socket.on('data', (data) => {
-        console.log('mensaje recibido del cliente: ' + data)
-        _message = data.toString();
-        console.log(_message)
-        socket.end();
-    })
-    socket.on('close', () => {
-        console.log('comunicación finalizada')
-    })
-    socket.on('error', (err) => {
-        console.log(err.message)
-    })
+  socket.on("data", (data) => {
+    console.log("mensaje recibido del cliente: " + data);
+    _message = data.toString();
+    console.log(_message);
+    socket.end();
+  });
+  socket.on("close", () => {
+    console.log("comunicación finalizada");
+  });
+  socket.on("error", (err) => {
+    console.log(err.message);
+  });
 });
 /*  server.listen({
     host: '192.168.1.84',
@@ -76,161 +83,207 @@ const server = net.createServer((socket) => {
     exclusive: true
 });  */
 //--------------------------server----------------
-app.use(express.static('web'));
+app.use(express.static("web"));
 
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname + '/web/templates/home.html'));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/web/templates/home.html"));
 });
 
 //------------------login----------------------
-app.get('/login', function (req, res) {
-    res.sendFile(path.join(__dirname + '/web/templates/loginp.html'));
+app.get("/login", function (req, res) {
+  res.sendFile(path.join(__dirname + "/web/templates/loginp.html"));
 });
-app.post('/login', (req, res) => {
-    var username = req.body.usuario;
-    var password = req.body.contraseña;
-    const admin = "admin";
-    console.log(username, password)
-    if (username && password) {
-        let sql = `SELECT l.username, l.password, r.rol from log l, rol r WHERE l.username LIKE '${username}' AND l.password LIKE '${password}' AND r.idrol = l.rol`;
-        let query = database.query(sql, (err, results) => {
-            if (results.length > 0) {
-                //console.log(results[0].rol)
-                if (err) {
-                    res.send('Incorrect Username and/or Password!');
-                }
-                else if (username == results[0].username && password == results[0].password && admin == results[0].rol) {
-                    req.session.loggedin = true;
-                    res.redirect('/dashboard');
-                }
-
-            }
-            else {
-                res.redirect('/login')
-                alert("Incorrect Username and/or Password!");
-            }
-
-        });
-
-    }
-
-
-
+app.post("/login", (req, res) => {
+  var username = req.body.usuario;
+  var password = req.body.contraseña;
+  const admin = "admin";
+  if (username && password) {
+    let sql = `SELECT l.username, l.password, r.rol from log l, rol r WHERE l.username LIKE '${username}' AND l.password LIKE '${password}' AND r.idrol = l.rol`;
+    let query = database.query(sql, (err, results) => {
+      if (results.length > 0) {
+        if (err) {
+          res.send("Incorrect Username and/or Password!");
+        } else if (
+          username == results[0].username &&
+          password == results[0].password &&
+          admin == results[0].rol
+        ) {
+          req.session.loggedin = true;
+          res.redirect("/dashboard");
+        }else {
+          alert("Incorrect Username and/or Password!");
+          res.redirect("/login")
+        }
+      } else {
+        res.redirect("/login");
+        alert("Incorrect Username and/or Password!");
+      }
+    });
+  }
 });
-
-
-
 
 //--------LIVE----------------
 app.get("/live", (req, res) => {
-    if (req.session.loggedin) {
-        res.sendFile(path.join(__dirname + '/web/templates/live.html'));
-    } else {
-        alert("PRIMERO DEBE INGRESAR")
-        res.redirect('/login')
-    }
+  if (req.session.loggedin) {
+    res.sendFile(path.join(__dirname + "/web/templates/live.html"));
+  } else {
+    alert("PRIMERO DEBE INGRESAR");
+    res.redirect("/login");
+  }
 });
 app.get("/live1", (req, res) => {
-    _message
-    res.end(JSON.stringify(_message));
+  _message;
+  res.end(JSON.stringify(_message));
 });
 
 //----------dashboard--------------------
-app.get('/da', function (req, res) {
-    let sql = 'SELECT r.rol, count(e.rol) as num FROM placas.entrada e, placas.rol r  where e.rol=r.idrol  group by e.rol having count(e.rol)>1 ORDER by e.idEntrada;'
-    let query = database.query(sql, (err, result) => {
-        if (err) throw err;
-        console.log(result)
-        res.end(JSON.stringify(result));
-    });
+app.get("/da", function (req, res) {
+  let sql =
+    "SELECT r.rol, count(e.rol) as num FROM placas.entrada e, placas.rol r  where e.rol=r.idrol  group by e.rol having count(e.rol)>1 ORDER by e.idEntrada;";
+  let query = database.query(sql, (err, result) => {
+    if (err) throw err;
+    //console.log(result);
+    res.end(JSON.stringify(result));
+  });
 });
 
-app.get('/dashboard', function (req, res) {
-    if (req.session.loggedin) {
-        res.sendFile(path.join(__dirname + '/web/templates/dashboard.html'));
-    } else {
-        alert("PRIMERO DEBE INGRESAR")
-        res.redirect('/login')
-    }
-
+app.get("/dashboard", function (req, res) {
+  if (req.session.loggedin) {
+    res.sendFile(path.join(__dirname + "/web/templates/dashboard.html"));
+  } else {
+    alert("PRIMERO DEBE INGRESAR");
+    res.redirect("/login");
+  }
 });
 //------------------tabla con usuarios-----------------------------s
-app.get('/usuarios', function (req, res) {
-    if (req.session.loggedin) {
-        let sql = 'SELECT * FROM log;'
-        let query = database.query(sql, (err, result) => {
-            if (err) throw err;
-            res.render('usuarios', { results: result });
-        });
-    } else {
-        alert("PRIMERO DEBE INGRESAR")
-        res.redirect('/login')
-    }
-});
-
-app.get('/delete/:idlog', (req, res) => {
-    const idlog = req.params.idlog;
-    let sql = 'DELETE FROM log WHERE idlog= ?'
-    let query = database.query(sql, [idlog], (err, result) => {
-        if (err) throw err;
-        //console.log(result)
-        res.redirect('/usuarios')
-    });
-});
-
-app.post('/api/crear', (req, res) => {
-    //console.log(req.params)
-    console.log(req.body)
-    const data = {
-        "pro_nombre": "esteban",
-        "pro_placa": "zzz300"
-    }
-    let username = req.body.usuario;
-    let password = req.body.contraseña;
-    if (username && password) {
-        let sql = `SELECT l.username, l.password, r.rol from log l, rol r WHERE l.username LIKE '${username}' AND l.password LIKE '${password}'`;
-        let query = database.query(sql, (err, results) => {
-            if (results.length > 0) {
-                //console.log(results[0].rol)
-                if (err) {
-                    console.log('Incorrect Username and/or Password!');
-                }
-                console.log("Login")
-                res.json([data])
-            }
-            else {
-                console.log('Incorrect Username and/or Password!');
-            }
-        });
-    };
-});
-
-app.post('/api/n_user', (req, res) => {
-    console.log(req.body)
-    let username = req.body.usuario;
-    let fullname = req.body.fullname;
-    let password = req.body.contraseña;
-    let placa = req.body.placa;
-    let rol = req.body.rol;
-    let visitante = "visitante";
-    let contratista = "contratista";
-    let trabajador = "trabajador"
-    if (rol == "visitante") {
-        rol = 1
-    } else if (rol == "trabajador") {
-        rol = 2
-    } else {
-        rol = 3
-    }
-    let sql = `INSERT INTO log  (username, password, fullname, placa, rol) VALUES ('${username}', '${password}', '${fullname}', '${placa}', '${rol}')`;
+app.get("/usuarios", function (req, res) {
+  if (req.session.loggedin) {
+    let sql =
+      "SELECT l.idlog, l.username, l.fullname, l.placa, l.estado, r.rol FROM log l, rol r WHERE l.rol = r.idrol;";
     let query = database.query(sql, (err, result) => {
-        if (err) throw err;
+      //console.log(result[1].rol);
+      //console.log(result.length);
+      let roles = [];
+      for (let rols in result) {
+        if (result[rols].rol == 1) {
+          roles.push("visitante");
+        } else if (result[rols].rol == 2) {
+          roles.push("Trabajador");
+        } else if (result[rols].rol == 3) {
+          roles.push("Contratista");
+        } else {
+          roles.push("Administrador");
+        }
+      }
+      //console.log(roles);
+      if (err) throw err;
+      res.render("usuarios", { results: result });
     });
-    const data = {
-        "pro_nombre": "esteban",
-        "pro_placa": "zzz300"
-    }
-    res.json([data])
+  } else {
+    alert("PRIMERO DEBE INGRESAR");
+    res.redirect("/login");
+  }
 });
 
+//Ruta de edición
+app.get("/edit/:idlog", function (req, res) {
+  const idlog = req.params.idlog;
+  let sql =
+    "SELECT l.idlog, l.username, l.fullname, l.placa, l.estado, r.rol FROM log l, rol r WHERE l.rol = r.idrol AND idlog = ?";
+  let query = database.query(sql, [idlog], (err, result) => {
+    if (err) throw err;
+    //console.log(result)
+    res.render("edit", { user: result[0] });
+  });
+});
+
+app.post("/update", (req, res) => {
+  let id = req.body.id;
+  let username = req.body.user;
+  let rol = req.body.rol;
+  let fullname = req.body.fullname;
+  let placa = req.body.placa;
+  let estado = req.body.estado;
+  let sql = "UPDATE log SET ? WHERE idlog = ?";
+  let query = database.query(
+    sql,
+    [
+      {
+        username: username,
+        fullname: fullname,
+        placa: placa,
+        rol: rol,
+        estado: estado,
+      },
+      id,
+    ],
+    (err, result) => {
+      if (err) throw err;
+      res.redirect("/usuarios");
+    }
+  );
+});
+
+app.get("/delete/:idlog", (req, res) => {
+  const idlog = req.params.idlog;
+  let sql = "DELETE FROM log WHERE idlog = ?";
+  let query = database.query(sql, [idlog], (err, result) => {
+    if (err) throw err;
+    //console.log(result)
+    res.redirect("/usuarios");
+  });
+});
+
+app.post("/api/crear", (req, res) => {
+  //console.log(req.params)
+  console.log(req.body);
+  const data = {
+    pro_nombre: "esteban",
+    pro_placa: "zzz300",
+  };
+  let username = req.body.usuario;
+  let password = req.body.contraseña;
+  if (username && password) {
+    let sql = `SELECT l.username, l.password, r.rol from log l, rol r WHERE l.username LIKE '${username}' AND l.password LIKE '${password}'`;
+    let query = database.query(sql, (err, results) => {
+      if (results.length > 0) {
+        //console.log(results[0].rol)
+        if (err) {
+          console.log("Incorrect Username and/or Password!");
+        }
+        console.log("Login");
+        res.json([data]);
+      } else {
+        console.log("Incorrect Username and/or Password!");
+      }
+    });
+  }
+});
+
+app.post("/api/n_user", (req, res) => {
+  console.log(req.body);
+  let username = req.body.usuario;
+  let fullname = req.body.fullname;
+  let password = req.body.contraseña;
+  let placa = req.body.placa;
+  let rol = req.body.rol;
+  let visitante = "visitante";
+  let contratista = "contratista";
+  let trabajador = "trabajador";
+  if (rol == "visitante") {
+    rol = 1;
+  } else if (rol == "trabajador") {
+    rol = 2;
+  } else {
+    rol = 3;
+  }
+  let sql = `INSERT INTO log  (username, password, fullname, placa, rol) VALUES ('${username}', '${password}', '${fullname}', '${placa}', '${rol}')`;
+  let query = database.query(sql, (err, result) => {
+    if (err) throw err;
+  });
+  const data = {
+    pro_nombre: "esteban",
+    pro_placa: "zzz300",
+  };
+  res.json([data]);
+});
